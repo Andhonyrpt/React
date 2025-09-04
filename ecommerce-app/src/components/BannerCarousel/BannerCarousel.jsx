d269732565f12723892f2e2e55eb70a0aa6f148d
Andhony <andhonyrpt@gmail.com>
2025-09-04 13:57:53 -0600

    Update

---
+++ ecommerce-app/src/components/BannerCarousel/BannerCarousel.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./BannerCarousel.css";
+
+function BannerCarousel({ banners }) {
+    return (
+        <div className="banner-carousel">
+            {banners.map((banner, index) => {
+                return (
+                    <div key={index} className="banner">
+                        <img src={banner.image} alt={banner.title} />
+                        <h2>{banner.title}</h2>
+                        <p>{banner.description}</p>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
+
+export default BannerCarousel;
